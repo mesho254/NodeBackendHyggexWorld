e59a6ed09d3dcfd022fcba190beb3eb425c785d1
Meshak Otieno <meshakotieno343@gmail.com>
2024-04-07 21:23:45 +0300

    chore: deleted file

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 5000,
-    jwtSecret: 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxMDMzNzA1OCwiaWF0IjoxNzEwMzM3MDU4fQ.DacGC0zELvQ5mRGO46iR3Au95ggv5xPi9icQCd3QXF8',
-    db: {
-      url: 'mongodb+srv://meshakotieno343:KeuhpXOwX6aX6yIZ@cluster0.rxchkjo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    },
-  };
-  
